5367b7c3672a00371ebe9cf85df6d9e0616524ad
GabrielMaurinaAmaral <gabrielmaurina2003@gmail.com>
2024-02-12 10:24:25 -0300

    first commit

---
+++ Meus_Codigos/R/1_Indrodução/Ajuste.r
@@ -0,0 +1,5 @@
+```{r}
+help(sd)
+head(iris)
+head(head)
+```
---
+++ Meus_Codigos/R/1_Indrodução/Exercicio.r
@@ -0,0 +1,31 @@
+ana = 8 
+paula = 12
+
+if(ana<paula) {
+    print('PAULA MAIS VELHOR QUE ANA')
+}
+
+A=BOD
+
+class(A)
+
+women
+head(women , n=10)
+
+iris
+iris[50:100,]
+
+
+iris
+iris[50:100,]
+plot(iris[50:100, c(2,3)])
+
+
+dim(CO2)[1]
+dim(CO2)[2]
+rownames(CO2)
+colnames(CO2)
+
+
+
+
